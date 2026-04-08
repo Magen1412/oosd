b3c03665ce7c052a1d738d6b5066c87577544b8a
Koshalz <bheemanaikokoshalkordas@gmail.com>
2026-04-08 20:08:17 +0400

    companydashboard

--- src/main/java/internship/companydashboard/Companydashboard.java
+++ src/main/java/internship/companydashboard/Companydashboard.java
@@ -12,6 +12,7 @@ import internship.choiceapplicationpage.ChoiceApplicationPage;
 import internship.dashboard.dao.CompanyDAO;
 import internship.dashboard.model.Company;
 import internship.editofferpage.EditOfferPage;
+import internship.profile.ProfilePage;
 
 public class Companydashboard extends JPanel {
 
@@ -55,11 +56,11 @@ public class Companydashboard extends JPanel {
 
         sidebar.add(Box.createVerticalStrut(20));
         sidebar.add(createSidebarButton("Dashboard", "companyDashboard"));
+        sidebar.add(createSidebarButton("Company Profile ", "CompanyProfile"));
         sidebar.add(createSidebarButton("Internship Offers", "AddOffer"));
         sidebar.add(createSidebarButton("View Applications", "ApplicationViewPage"));
         sidebar.add(createSidebarButton("Edit Internship Offer ", "EditOfferPage"));
         sidebar.add(createSidebarButton("Schedule interview ", "internshipschedule"));
-        //sidebar.add(createSidebarButton("Interview Scheduling Page ", "Profile"));
 
         sidebar.add(Box.createVerticalGlue());
 
@@ -221,7 +222,7 @@ public class Companydashboard extends JPanel {
             mainContent.add(new ApplicationStatusPage(mainContent, cl), "ApplicationStatusPage");
             mainContent.add(new internshipschedule(cl, mainContent), "internshipschedule");
             mainContent.add(new EditOfferPage(cl,mainContent), "EditOfferPage");
-           // mainContent.add(new CompanyProfilePage(mainContent, cl), "Approve/Reject Page");
+            mainContent.add(new CompanyProfile(cl , mainContent), "companyProfile");
 
             frame.add(mainContent);
             frame.setLocationRelativeTo(null);
